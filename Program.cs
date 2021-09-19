a988b5263e06945771cecd53fc72aff096565955
Prakash <prakash.manit@gmail.com>
2021-09-19 22:34:14 +0530

    Added queue changes

--- Program.cs
+++ Program.cs
@@ -1,180 +1,20 @@
-﻿using System;
+﻿using DSnA;
+using System;
 
 namespace Singleton
 {
     class Program
-    {
-        static int top = -1;
-        static int[] stack = new int[10] { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
-        static int MAXSIZE = 10;
-        static int data = default;        
+    {   
         static void Main(string[] args)
         {
             bool showMenu = true;
             while (showMenu)
             {
-                showMenu = MainMenu();
+                //showMenu = StackCustom.MainMenu();
+                showMenu = QueueCustom.MainMenu();
             }
         }
 
-        static bool MainMenu()
-        {
-            Console.Write("A menu driven program for stack\n");
-            Console.Write("------------------------------------------------");
-            Console.Write("\nHere are the options :\n");
-            Console.Write("1-Push.\n2-Pop.\n3-Peek.\n4-List.\n5-MakeEmpty.\n6-Exit.\n");
-            Console.Write("\nInput your choice :");
-
-            if (int.TryParse(Console.ReadLine(), out int option))
-            {
-                switch (option)
-                {
-                    case 1:
-                        Console.WriteLine("How many members you wants to push? (put any number between 1 and 10)");
-                        if (int.TryParse(Console.ReadLine(), out int count))
-                        {
-
-                            if (count > 0 && count <= 10)
-                            {
-                                Console.WriteLine("Please enter the number now!!");
-                                for (int i = 0; i < count; i++)
-                                {
-                                    if (int.TryParse(Console.ReadLine(), out int data))
-                                        Push(data);
-                                }
-                            }
-                            else
-                            {
-                                Console.WriteLine("Please enter the number between 1 and 10!!");
-                                count = Convert.ToInt32(Console.ReadLine());
-                                if (count > 0 && count <= 10)
-                                {
-                                    Console.WriteLine("Please enter the number now!!");
-                                    for (int i = 0; i < count; i++)
-                                    {
-                                        if (int.TryParse(Console.ReadLine(), out int data))
-                                            Push(data);
-                                    }
-                                }
-                                else
-                                {
-                                    Console.WriteLine("You have exceeded the number of wrong attempt. Good Bye!!");
-                                    return false;
-                                }
-
-                            }
-
-                        }
-                        return true;
-
-                    case 2:
-                        Pop();
-                        return true;
-                    case 3:
-                        Console.WriteLine($"Top member of stack is: {Peek()}.\n");
-                        return true;
-                    case 4:
-                        ListStackMembers();
-                        return true;
-                    case 5:
-                        MakeEmpty();
-                        return true;
-                    case 6:
-                        return false;
-                    default:
-                        return true;
-                }                
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        static void MakeEmpty()
-        {
-            for (int i = stack.Length - 1; i >= 0; i--)
-                stack[i] = -1;
-        }
-
-        static void ListStackMembers()
-        {
-            int tempCounter = 0;
-            for (int i = stack.Length - 1; i >= 0; i--)
-            {
-                
-                if (stack[i] > -1)
-                {
-                    Console.WriteLine(stack[i]);
-                    ++tempCounter;
-                }
-                else
-                {
-                    continue;
-                }
-            }
-
-            if (tempCounter == 0)
-            {
-                Console.WriteLine("Could not retrieve data, Stack is empty.\n");
-            }
-
-        }
-
-        static void Push(int data)
-        {
-            if (!isFull())
-            {
-                top = top + 1;
-                stack[top] = data;
-                Console.WriteLine($"Member {data} is pushed.");
-            }
-            else
-            {
-                Console.WriteLine("Could not insert data, Stack is full.\n");
-            }
-        }
-
-        static void Pop()
-        {
-
-            if (!isEmpty())
-            {
-                data = stack[top];
-                stack[top] = -1;
-                top = top - 1;
-                Console.WriteLine($"Member {data} is popped.\n");
-
-            }
-            else
-            {
-                Console.WriteLine("Could not retrieve data, Stack is empty.\n");
-            }
-        }
-
-        static int Peek()
-        {
-            if (top != -1)
-                return stack[top];
-            else
-                return -1;
-
-        }
-
-        static bool isFull()
-        {
-            if (top == MAXSIZE)
-                return true;
-            else
-                return false;
-        }
-
-        static bool isEmpty()
-        {
-            if (top == -1)
-                return true;
-            else
-                return false;
-        }
+        
     }
 }
---
+++ QueueCustom.cs
@@ -0,0 +1,178 @@
+﻿using System;
+
+namespace DSnA
+{
+    class QueueCustom
+     {
+        static int front = 0;
+        static int rear = 0;        
+        const int MAXSIZE = 10;
+        static int[] queue = new int[MAXSIZE] { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+        static int data = default;
+        public static bool MainMenu()
+        {
+            Console.Write("A menu driven program for queue\n");
+            Console.Write("------------------------------------------------");
+            Console.Write("\nHere are the options :\n");
+            Console.Write("1-Enqueue.\n2-Dequeue.\n3-Peek.\n4-List.\n5-MakeEmpty.\n6-Exit.\n");
+            Console.Write("\nInput your choice :");
+
+            if (int.TryParse(Console.ReadLine(), out int option))
+            {
+                switch (option)
+                {
+                    case 1:
+
+                        if (!isFull())
+                        {
+                            Console.WriteLine("How many members you wants to enqueue? (put any number between 1 and 10)");
+                            if (int.TryParse(Console.ReadLine(), out int count))
+                            {
+
+                                if (count > 0 && count <= 10)
+                                {
+                                    Console.WriteLine("Please enter the number now!!");
+                                    for (int i = 0; i < count; i++)
+                                    {
+                                        if (int.TryParse(Console.ReadLine(), out int data))
+                                            Enqueue(data);
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Please enter the number between 1 and 10!!");
+                                    count = Convert.ToInt32(Console.ReadLine());
+                                    if (count > 0 && count <= 10)
+                                    {
+                                        Console.WriteLine("Please enter the number now!!");
+                                        for (int i = 0; i < count; i++)
+                                        {
+                                            if (int.TryParse(Console.ReadLine(), out int data))
+                                                Enqueue(data);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("You have exceeded the number of wrong attempt. Good Bye!!");
+                                        return false;
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Could not insert data, Queue is full.\n");
+                        }                        
+
+                        return true;
+                    case 2:
+                        Dequeue();
+                        return true;
+                    case 3:
+                        Console.WriteLine($"Top member of queue is: {Peek()}.\n");
+                        return true;
+                    case 4:
+                        ListQueueMembers();
+                        return true;
+                    case 5:
+                        MakeEmpty();
+                        return true;
+                    case 6:
+                        return false;
+                    default:
+                        return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static void MakeEmpty()
+        {
+            for (int i = queue.Length - 1; i >= 0; i--)
+                queue[i] = -1;
+        }
+
+        static void ListQueueMembers()
+        {
+            int tempCounter = 0;
+            for (int i = front; i < rear; i++)
+            {
+
+                if (queue[i] > -1)
+                {
+                    Console.WriteLine(queue[i]);
+                    ++tempCounter;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            if (tempCounter == 0)
+            {
+                Console.WriteLine("Could not retrieve data, Queue is empty.\n");
+            }
+
+        }
+
+        static void Enqueue(int data)
+        {
+            if (!isFull())
+            {
+                queue[rear] = data;
+                ++rear;
+                Console.WriteLine($"Member {data} is en-queued.");
+            }
+            else
+            {
+                Console.WriteLine("Could not insert data, Queue is full.\n");
+            }
+        }
+
+        static void Dequeue()
+        {
+
+            if (!isEmpty())
+            {
+                Console.WriteLine($"Member {queue[front]} is de-queued.\n");
+                for (int i = front; i < rear - 1; i++)
+                {
+                    queue[i] = queue[i + 1];
+                }                
+                
+                queue[rear-1] = -1;
+                --rear;
+
+            }
+            else
+            {
+                Console.WriteLine("Could not retrieve data, Queue is empty.\n");
+            }
+        }
+
+        static int Peek()
+        {
+            return queue[front];
+        }
+
+        static bool isFull()
+        {
+            if (rear == MAXSIZE)
+                return true;
+            else
+                return false;
+        }
+
+        static bool isEmpty()
+        {
+            if (front == rear)
+                return true;
+            else
+                return false;
+        }
+    }
+}
