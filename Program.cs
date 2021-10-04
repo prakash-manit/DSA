11e962237ab19b39721f1ddabd606c1b0c0b7248
Prakash <prakash.manit@gmail.com>
2021-10-04 08:25:49 +0530

    Added new class

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using DSnA;
 using System;
+using System.Collections.Generic;
 
 namespace Singleton
 {
@@ -7,12 +8,26 @@ namespace Singleton
     {   
         static void Main(string[] args)
         {
-            bool showMenu = true;
-            while (showMenu)
-            {
-                //showMenu = StackCustom.MainMenu();
-                showMenu = QueueCustom.MainMenu();
-            }
+            //bool showMenu = true;
+            //while (showMenu)
+            //{
+            //    //showMenu = StackCustom.MainMenu();
+            //    //showMenu = QueueCustom.MainMenu();
+            //}
+
+            /* Create a queue with items 1 2 3*/
+            QueueUsingStack q = new QueueUsingStack();
+            q.stack1 = new Stack<int>();
+            q.stack2 = new Stack<int>();
+            QueueUsingStack.enQueue(q, 1);
+            QueueUsingStack.enQueue(q, 2);
+            QueueUsingStack.enQueue(q, 3);
+
+            /* Dequeue items */
+            Console.Write(QueueUsingStack.deQueue(q) + " ");
+            Console.Write(QueueUsingStack.deQueue(q) + " ");
+            Console.WriteLine(QueueUsingStack.deQueue(q) + " ");
+
         }
 
         
---
+++ QueueUsingStack.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DSnA
+{
+    class QueueUsingStack
+    {
+        public Stack<int> stack1;
+        public Stack<int> stack2;
+
+        /* Function to push an item to stack*/
+        static void push(Stack<int> top, int new_data)
+        {
+            // Push the data onto the stack
+            top.Push(new_data);
+        }
+
+        /* Function to pop an item from stack*/
+        static int pop(Stack<int> top)
+        {
+            /*If stack is empty then error */
+            if (top.Count == 0)
+            {
+                Console.WriteLine("Stack is empty");
+                Environment.Exit(0);
+            }
+
+            // pop the data from the stack
+            return top.Pop();
+        }
+
+        // Function to enqueue an item to the queue
+        public static void enQueue(QueueUsingStack q, int x)
+        {
+            push(q.stack1, x);
+        }
+
+        /* Function to deQueue an item from queue */
+        public static int deQueue(QueueUsingStack q)
+        {
+            int x;
+
+            /* If both stacks are empty then error */
+            if (q.stack1.Count == 0 && q.stack2.Count == 0)
+            {
+                Console.WriteLine("Queue is empty");
+                Environment.Exit(0);
+            }
+
+            /* Move elements from stack1 to stack 2 only if
+            stack2 is empty */
+            if (q.stack2.Count == 0)
+            {
+                while (q.stack1.Count != 0)
+                {
+                    x = pop(q.stack1);
+                    push(q.stack2, x);
+                }
+            }
+            x = pop(q.stack2);
+            return x;
+        }
+    }
+}
