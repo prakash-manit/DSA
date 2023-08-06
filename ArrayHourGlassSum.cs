caf2bf8522f91345875e050e9b109bffb0d8e6a4
Prakash <prakash.manit@gmail.com>
2023-08-06 17:39:27 +0530

    Updated the file

--- ArrayHourGlassSum.cs
+++ ArrayHourGlassSum.cs
@@ -26,28 +26,55 @@ namespace DSnA
 
         private static int GethourglassSum(List<List<int>> arr)
         {
-            //int[,] arr = list.Select(a => a.ToArray()).ToArray();
-
             int sum = 0;
-            
-            for (int i = 0; i < arr.Count-2; i++)
+
+            //Using array of arrays
+            for (int i = 0; i < arr.Count - 2; i++)
             {
-                for (int j = 0; j < arr[i].Count-2; j++)
+                for (int j = 0; j < arr[i].Count - 2; j++)
                 {
-                    int tempSum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1]+ arr[i+2][j+2];
-                    
+                    int tempSum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+
                     if (i == 0 && j == 0)
                     {
                         sum = tempSum; //Initialize for the first time
                     }
-                   
+
                     if (tempSum > sum)
                     {
                         sum = tempSum;
                     }
                 }
             }
-            
+
+            ////Using 2D Array
+            //int[,] arr2 = new int[arr.Count, arr[0].Count];
+            //for (int i = 0; i < arr.Count; i++)
+            //{
+            //    for (int j = 0; j < arr[i].Count; j++)
+            //    {
+            //        arr2[i, j] = arr[i][j];
+            //    }
+            //}
+
+            //for (int i = 0; i < arr2.GetLength(0) - 2; i++)
+            //{
+            //    for (int j = 0; j < arr2.GetLength(1) - 2; j++)
+            //    {
+            //        int tempSum = arr2[i, j] + arr2[i, j + 1] + arr2[i, j + 2] + arr2[i + 1, j + 1] + arr2[i + 2, j] + arr2[i + 2, j + 1] + arr2[i + 2, j + 2];
+
+            //        if (i == 0 && j == 0)
+            //        {
+            //            sum = tempSum; //Initialize for the first time
+            //        }
+
+            //        if (tempSum > sum)
+            //        {
+            //            sum = tempSum;
+            //        }
+            //    }
+            //}
+
             return sum;
         }
     }
