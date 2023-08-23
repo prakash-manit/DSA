1e510ec2a2fef99d9c4b45885f2951fda9c6cac9
Prakash <prakash.manit@gmail.com>
2023-08-23 13:34:44 +0530

    Updated the code

--- ArrayManipulation.cs
+++ ArrayManipulation.cs
@@ -42,20 +42,41 @@ namespace DSnA
         //    return result.Max();
         //}
 
-        private static long arrayManipulation(int n, List<List<int>> queries)
+        private static long arrayManipulation(int n, List<List<int>> queries) // All 16 test cases passed
         {
-
-            long[] result = new long[n];
+            long[] result  = new long[n];
 
             for (int i = 0; i < queries.Count; i++)
             {
-                for (int j = queries[i][0]; j <= queries[i][1]; j++)
+                int start = queries[i][0] - 1;
+                int end = queries[i][1];
+                int value =  queries[i][2];
+
+                result[start] = result[start] + value;
+                if (end < n)
                 {
-                    result[j - 1] = result[j - 1] + queries[i][2];
+                    result[end] = result[end] - value;
                 }
             }
 
+            for (int i = 1; i < n; i++)
+            {
+                result [i] = result[i] + result[i-1]; //prefix sum approach
+            }
+
             return result.Max();
+
+            //long max = 0;
+            //long prefix_sum = 0;
+            //for (int i = 0; i < n; i++)
+            //{
+            //    prefix_sum += result[i];
+            //    if (prefix_sum > max)
+            //    {
+            //        max = prefix_sum;
+            //    }
+            //}
+            //return max;
         }
     }
 }
