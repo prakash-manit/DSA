e85159d2cdd54279cf9082a174442fbd6ad411e5
Prakash <prakash.manit@gmail.com>
2024-02-05 11:13:50 +0530

    Updated file

--- 1.String_Array/DelArrEle.cs
+++ 1.String_Array/DelArrEle.cs
@@ -7,23 +7,27 @@ namespace DSA
         // Driver Code
         public static void Driver()
         {
-            int i;
             int[] nums = { 10, 50, 30, 40, 20, 30 };
-            //int[] arr2 = { 10, 50, 10, 40, 20 };
 
             int delEle = 30;
 
-            Console.Write("Array before deletion ");
-            for (i = 0; i < nums.Length; i++)
+            Console.Write("Array before deletion\n");
+            for (int i = 0; i < nums.Length; i++)
                 Console.Write(nums[i] + " ");
-            Console.WriteLine();
 
             // Delete function call
-            int remCount = deleteElement(nums, delEle);
+            int remCount = deleteElement2(nums, delEle);
 
-            Console.Write("Array after deletion ");
-            for (i = 0; i < remCount; i++)
-                Console.Write(nums[i] + " ");
+            if (remCount < nums.Length)
+            {
+                Console.Write("\nArray after deletion\n");
+                for (int i = 0; i < remCount; i++)
+                    Console.Write(nums[i] + " ");
+            }
+            else
+            {
+                Console.WriteLine("\nElement not found!!");
+            }
         }
 
         private static int GetIndex(int[] nums, int delEle)
@@ -43,8 +47,8 @@ namespace DSA
             //check if element to delete is present in the array
             int idx = GetIndex(nums, delEle);
             if (idx == -1)
-            {
-                Console.WriteLine("Element not found!!");
+            {                
+                return nums.Length;
             }
             else
             {
@@ -52,8 +56,8 @@ namespace DSA
                 {
                     nums[i] = nums[i + 1];
                 }
-            }
-            return nums.Length - 1;
+                return nums.Length - 1;
+            }            
         }
 
         private static int deleteElement2(int[] nums, int delEle)
